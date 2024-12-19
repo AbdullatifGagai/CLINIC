0bc5486937d65fd698ca9f7f67527632020a2f46
AbdullatifGagai <69461915+AbdullatifGagai@users.noreply.github.com>
2024-12-19 06:52:26 -0800

    update

---
+++ ZCLINIC/Areas/Master/Models/PatientReg.cs
@@ -0,0 +1,201 @@
+using DatabaseTVP_Core;
+using Logger_Core;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace ZCLINIC.Areas.Master.Models
+{
+    public class PatientReg
+    {
+        private const string MODULE = "Master";
+
+
+
+
+
+
+        [TVP]
+        public int MRId { get; set; }
+        [TVP]
+        public int MrGr { get; set; }
+        [TVP]
+        public string Name { get; set; }
+        [TVP]
+        public string Sint { get; set; }
+        [TVP]
+        public string  Fname { get; set; }
+
+        [TVP]
+        public string  Cellno { get; set; }
+
+        [TVP]
+        public string  Phone { get; set; }
+        
+        [TVP]
+        public string  CNic { get; set; }
+        
+        
+        
+        
+        [TVP]
+        public string  RAdd { get; set; }
+        [TVP]
+        public string  Bdate { get; set; }
+        [TVP]
+        public string  Age { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //properties for model
+        [TVP]
+        public int TenantId { get; set; }
+
+        [TVP]
+        public string AppId { get; set; }
+
+        [TVP]
+        public int CreatedBy { get; set; }
+
+        [TVP]
+        public int ModifiedBy { get; set; }
+
+        //View only properties
+        public string ReturnMessage { get; set; }
+
+        public string Master_PatientRegSave(BValues bv, string ip, string cInfo)
+        {
+            var parameters = new { x = this };
+
+            try
+            {
+                TenantId = bv.TenantId; AppId = bv.AppId; CreatedBy = bv.UserId; ModifiedBy = bv.UserId;
+
+                string message = DataBase.ExecuteQuery<PatientReg>(parameters, Connection.GetConnection()).FirstOrDefault().ReturnMessage;
+
+                Loging.DB_Log(eLogType.Log_Posetive, "", parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                Loging.DB_Log(eLogType.Log_Negative, ex.Message, parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return ex.Message;
+            }
+        }
+
+        public string Master_PatientRegUpdate(BValues bv, string ip, string cInfo)
+        {
+            var parameters = new { x = this };
+
+            try
+            {
+                TenantId = bv.TenantId; AppId = bv.AppId; ModifiedBy = bv.UserId;
+
+                string message = DataBase.ExecuteQuery<PatientReg>(parameters, Connection.GetConnection()).FirstOrDefault().ReturnMessage;
+
+                Loging.DB_Log(eLogType.Log_Posetive, "", parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                Loging.DB_Log(eLogType.Log_Negative, ex.Message, parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return ex.Message;
+            }
+        }
+
+        public PatientReg Master_PatientRegGetById(int id, BValues bv, string ip, string cInfo)
+        {
+            var parameters = new { bv.UserId, bv.TenantId, bv.AppId, Id = id };
+
+            try
+            {
+                PatientReg record = DataBase.ExecuteQuery<PatientReg>(parameters, Connection.GetConnection()).FirstOrDefault();
+
+                Loging.DB_Log(eLogType.Log_Posetive, "", parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return record;
+            }
+            catch (Exception ex)
+            {
+                Loging.DB_Log(eLogType.Log_Negative, ex.Message, parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return null;
+            }
+        }
+
+        public List<PatientReg> Master_PatientRegGetAll(BValues bv, string ip, string cInfo)
+        {
+            var parameters = new { bv.UserId, bv.TenantId, bv.AppId };
+
+            try
+            {
+                List<PatientReg> records = DataBase.ExecuteQuery<PatientReg>(parameters, Connection.GetConnection());
+
+                Loging.DB_Log(eLogType.Log_Posetive, "", parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return records;
+            }
+            catch (Exception ex)
+            {
+                Loging.DB_Log(eLogType.Log_Negative, ex.Message, parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return null;
+            }
+        }
+
+        public DataTable Master_PatientRegGetAllTable(BValues bv, string ip, string cInfo)
+        {
+            var parameters = new { bv.UserId, bv.TenantId, bv.AppId };
+
+            try
+            {
+                DataTable dataTable = DataBase.ExecuteforReport(parameters, Connection.GetConnection(), "Master_PatientRegGetAllTable");
+
+                Loging.DB_Log(eLogType.Log_Posetive, "", parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                Loging.DB_Log(eLogType.Log_Negative, ex.Message, parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return null;
+            }
+        }
+
+        public string Master_PatientRegDelete(int id, BValues bv, string ip, string cInfo)
+        {
+            var parameters = new { bv.UserId, bv.TenantId, bv.AppId, Id = id };
+
+            try
+            {
+                string message = DataBase.ExecuteQuery<PatientReg>(parameters, Connection.GetConnection()).FirstOrDefault().ReturnMessage;
+
+                Loging.DB_Log(eLogType.Log_Posetive, "", parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return message;
+            }
+            catch (Exception ex)
+            {
+                Loging.DB_Log(eLogType.Log_Negative, ex.Message, parameters, "", MODULE, Connection.GetLogConnection(), cInfo, ip, bv.UserId);
+
+                return ex.Message;
+            }
+        }
+    }
+}
